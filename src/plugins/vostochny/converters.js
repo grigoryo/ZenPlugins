d3abd6ba6bf68daf321084da57c5ec0af920b94f
Grigory Osipenkov <grigory.osipenkov@gmail.com>
2018-11-24 02:57:45 +1000

    [vostochny] converters rewritten for better readability and scalability

--- src/plugins/vostochny/converters.js
+++ src/plugins/vostochny/converters.js
@@ -1,61 +1,124 @@
 import _ from 'lodash'
 import moment from 'moment'
 
-// TODO: remove comments
-// response.object.accounts[]
-// .id -> id
-// .name -> title
-// .number -> syncID +
-//   .limits[].cards[].num -> syncID
-// .currency.code -> instrument
-// .limits === [] -> type = 'checking'
-// .limits === [{...}, ...] -> type = 'ccard'
-// .availableWhenPay -> balance
-// -> startBalance
-// -> creditLimit
-// -> savings
 export function convertAccounts (accounts) {
-  return accounts.map(account => ({
-    id: _.get(account, 'id').toString(),
-    title: _.get(account, 'name').toString(),
-    syncID: [
-      _.get(account, 'number').toString().slice(-4),
-      _.get(account, 'limits[0].cards[0].num').toString().slice(-4)
-    ],
-    // TODO: compare account currency code with operation currency code
-    instrument: _.get(account, 'currency.code').toString(),
-    balance: Number(_.get(account, 'availableWhenPay')),
-    // TODO: need to find ways for determining other account types
-    type: (x => Array.isArray(x) && x.length > 0)(_.get(account, 'limits'))
-      ? 'ccard'
-      : 'checking'
-    // TODO: not all fields are accured from reverse engineered data
-  }))
+  return accounts.map(account => {
+    // TODO: remove: console.log(account)
+
+    let flat = {
+      id: _.get(account, 'id'),
+      name: _.get(account, 'name'),
+      number: _.get(account, 'number'),
+      limits0Cards0Num: _.get(account, 'limits[0].cards[0].num'),
+      currencyCode: _.get(account, 'currency.code'),
+      availableWhenPay: _.get(account, 'availableWhenPay'),
+      limits: _.get(account, 'limits')
+    }
+
+    let isValid =
+      _.isInteger(flat.id) &&
+      _.isString(flat.name) && flat.name.length > 0 &&
+      _.isString(flat.number) && flat.number.length === 20 &&
+      (
+        flat.limits0Cards0Num === undefined ||
+        (
+          _.isString(flat.limits0Cards0Num) &&
+          flat.limits0Cards0Num.length === 19
+        )
+      ) &&
+      _.isString(flat.currencyCode) &&
+      // TODO: flat.currencyCode === 'RUB' &&
+      _.isFinite(flat.availableWhenPay) && flat.availableWhenPay >= 0 &&
+      // TODO: fail if creditWhenPay !== availableWhenPay
+      _.isArray(flat.limits)
+
+    if (!isValid) throw new Error('Unexpected API answer')
+
+    let result = {
+      id: _.toString(flat.id),
+      title: flat.name,
+      syncID: (a => a.filter(x => x !== ''))([
+        flat.number.slice(-4),
+        _.toString(flat.limits0Cards0Num).slice(-4)
+      ]),
+
+      // TODO: compare account currency code with operation currency code
+      instrument: flat.currencyCode,
+      // TODO: need to find ways for determining other account types
+      type: flat.limits.length > 0 ? 'ccard' : 'checking',
+
+      balance: flat.availableWhenPay,
+      startBalance: undefined,
+      creditLimit: undefined,
+
+      savings: undefined,
+
+      capitalization: undefined,
+      percent: undefined,
+      startDate: undefined,
+      endDateOffset: undefined,
+      endDateOffsetInterval: undefined,
+      payoffStep: undefined,
+      payoffInterval: undefined
+    }
+    return result
+  })
 }
 
 export function convertOperations (operations) {
-  // TODO: generate more operation types in examples
   return operations.map(operation => {
-    // TODO: throw if not in RUR
+    // TODO: remove: console.log(operation)
+
+    // TODO: parse each known operation type separately
+
+    let flat = {
+      id: _.get(operation, 'id'),
+      accountId: _.get(operation, 'accountId'),
+      pfmCategoryTOKind: _.get(operation, 'pfmCategoryTO.kind'),
+      amount: _.get(operation, 'amount'),
+      mccCodeAndCategory: _.get(operation, 'mccCodeAndCategory'),
+      operDateTime: _.get(operation, 'operDateTime'),
+      hold: _.get(operation, 'hold')
+      // TODO: currencyCode: _.get(operation, 'currency.code')
+    }
+
+    // TODO: remove: console.log(flat)
+
+    let isValid =
+      _.isInteger(flat.id) &&
+      _.isInteger(flat.accountId) &&
+      (
+        flat.pfmCategoryTOKind === '+' ||
+        flat.pfmCategoryTOKind === '-' ||
+        flat.pfmCategoryTOKind === '0'
+      ) &&
+      _.isFinite(flat.amount) &&
+      _.isString(flat.mccCodeAndCategory) && // TODO: some number in it
+      _.isString(flat.operDateTime) && // TODO: flat.operDateTime is date in string
+      _.isBoolean(flat.hold)
+      // TODO: this.currencyCode === 'RUR'
+
+    if (!isValid) throw new Error('Unexpected API answer')
+
     let result = {
-      id: _.get(operation, 'id').toString(),
-      incomeBankID: _.get(operation, 'id').toString(),
-      incomeAccount: _.get(operation, 'accountId').toString(),
-      income: _.get(operation, 'pfmCategoryTO.kind') === '+'
-        ? Number(_.get(operation, 'amount'))
-        : 0,
-      outcomeBankID: _.get(operation, 'id').toString(),
-      outcomeAccount: _.get(operation, 'accountId').toString(),
-      outcome: _.get(operation, 'pfmCategoryTO.kind') === '-'
-        ? Number(_.get(operation, 'amount'))
-        : 0,
+      id: _.toString(flat.id),
+      incomeBankID: _.toString(flat.id),
+      incomeAccount: _.toString(flat.accountId),
+      income: flat.pfmCategoryTOKind === '+' ? flat.amount : 0.0,
+      outcomeBankID: _.toString(flat.id),
+      outcomeAccount: _.toString(flat.accountId),
+      outcome: flat.pfmCategoryTOKind === '-'
+        ? -flat.amount
+        : (flat.pfmCategoryTOKind === '0' ? -flat.amount : 0.0),
 
-      mcc: Number(_.get(operation, 'mccCodeAndCategory').replace('MCC ', '')),
-      payee: undefined, // TODO: parse details
+      mcc: (x => x !== '' ? _.toInteger(x) : undefined)(
+        flat.mccCodeAndCategory.replace('MCC ', '')
+      ), // TODO: get mcc correctly or fail
+      payee: undefined, // TODO: parse "details"
 
-      date: moment(_.get(operation, 'operDateTime'), 'DD.MM.YYYY HH:mm')
-        .utcOffset('+03:00', true),
-      hold: Boolean(_.get(operation, 'hold')),
+      date: moment(flat.operDateTime, 'DD.MM.YYYY HH:mm')
+        .utcOffset('+03:00', true).toDate(), // TODO: more clean way
+      hold: flat.hold,
 
       opIncome: undefined,
       opIncomeInstrument: undefined,
